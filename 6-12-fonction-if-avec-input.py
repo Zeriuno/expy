a755d1b9292b5f9ba08321b0df8737edfb30a5e8
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-03 17:55:20 +0200

    Tentative qui ne marche pas: il faut convertir 'a' en entier

---
+++ 6-12-fonction-if-avec-input.py
@@ -0,0 +1,17 @@
+a=input('''Saisir un chiffre: ''')
+
+def yolo(a):
+    """
+    :param a: integer submitted by the user
+    :return: nothing is returned, just a print screen
+    """
+b = ('''LO''')
+c = ('!!!')
+d = (''':'(''')
+
+if(a > 1):
+    print('''YO'''+b+c)
+else:
+    print('''SO'''+b+d)
+
+yolo(a)
